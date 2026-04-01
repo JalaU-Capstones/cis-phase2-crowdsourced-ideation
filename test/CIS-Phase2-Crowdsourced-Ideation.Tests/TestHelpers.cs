d4c65b91748d2a3ef724cd57c0f75af69269b4e8
Gabriel Mamani Sandoval <gabriel.mamani0125@jala.university>
2026-04-01 10:28:16 -0400

    test(auth): add packages and JWT token helper

--- test/CIS-Phase2-Crowdsourced-Ideation.Tests/TestHelpers.cs
+++ test/CIS-Phase2-Crowdsourced-Ideation.Tests/TestHelpers.cs
@@ -1,4 +1,6 @@
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CIS.Phase2.CrowdsourcedIdeation.Tests;
 
@@ -6,8 +8,28 @@ public static class TestHelpers
 {
     public static ClaimsPrincipal CreateClaimsPrincipal(string userId)
     {
-        var claims = new[] { new Claim("sub", userId) };
+        var claims   = new[] { new Claim("sub", userId) };
         var identity = new ClaimsIdentity(claims, "Test");
         return new ClaimsPrincipal(identity);
+    }
+    
+    public static string GenerateJwtToken(
+        string base64Secret,
+        string username,
+        int expiresInMinutes = 60)
+    {
+        var keyBytes    = Convert.FromBase64String(base64Secret);
+        var signingKey  = new SymmetricSecurityKey(keyBytes);
+        var credentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject            = new ClaimsIdentity(new[] { new Claim("sub", username) }),
+            Expires            = DateTime.UtcNow.AddMinutes(expiresInMinutes),
+            SigningCredentials = credentials
+        };
+
+        var handler = new JwtSecurityTokenHandler();
+        return handler.WriteToken(handler.CreateToken(tokenDescriptor));
     }
 }
